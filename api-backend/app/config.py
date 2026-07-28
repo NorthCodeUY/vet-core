bfd2feb086ac1d4cfeeb5f69d2bc2cc8c350ac0b
Ary Gimenez <argi.prog@gmail.com>
2026-07-28 17:35:47 -0300

    Sigo Trabajando en el entorno

--- api-backend/app/config.py
+++ api-backend/app/config.py
@@ -1,8 +1,9 @@
 # api-backend/app/config.py
 from pydantic_settings import BaseSettings  #  Importamos la clase BaseSettings del modulo pydantic_settings para poder crear variables de entorno 
+from pydantic_settings import SettingsConfigDict # Importamos la clase SettingsConfigDict para poder configurar el cargador
+
 import os # Importamos el modulo os para poder trabajar con rutas y archivos 
 import socket # Importamos el modulo socket para poder obtener la direccion ip local
-from pydantic_settings import SettingsConfigDict # Importamos la clase SettingsConfigDict para poder configurar el cargador
 
 def obtener_ip_local():
     """Detecta la IP privada de la máquina en la red local (WiFi/Ethernet)"""
@@ -42,28 +43,36 @@ class Settings(BaseSettings):
         env_file=ENV_PATH, # Le dice a Pydantic que busque el archivo .env
         extra='ignore', # Ignora variables no definidas
         env_file_encoding='utf-8' # Codificacion de utf-8
-    )
-
-    # <!> Version vieja sacar 
-    # class Config:
-    #     """ Configuración del entorno """
-    #     env_file = ENV_PATH # Le dice a Pydantic que busque el archivo .env  
-    #     extra = 'ignore'  
+    )  
 
 settings = Settings() # Instanciamos la configuración inicial desde el .env
 
 
+# --- LÓGICA DE DETECCIÓN DE ENTORNO ---
+is_docker = os.getenv("IS_DOCKER", "false").lower() == "true"
+origins_list = []
+allow_credentials = False
+
 #  LÓGICA DINÁMICA DE BASE_URL
 # Si estamos en desarrollo, ignoramos lo que diga el .env y construimos la URL con la IP real
 if settings.env_mode == "development":
-    ip_local = obtener_ip_local()
-    # Sobrescribimos base_url para que los QR e imágenes funcionen en toda la LAN
-    # Usamos el puerto 8001 que es el que definiste para tu backen
-    settings.base_url = f"http://{ip_local}:{settings.app_port}" 
-    # Forzamos orígenes abiertos para no renegar con CORS en LAN/Túneles
-    origins_list = ["*"]
-    allow_credentials = False 
-else:
+    # <!> Aca tengo una duda esto va a andar en el entorno bakend contenedor 
+    # fronten local pqero que pasa cuando es ambos en contenedor andara o no hay que refernciar 
+    # a otra cosa ??
+    host= "" # Host del backend
+    if is_docker:
+        host= "localhost" # enra atraves de el host local
+    else:
+        host = obtener_ip_local() # ip local del host
+    
+    
+    # 2. CORS en Desarrollo
+    origins_list = ["*"] # Abre la comunicacion con cualquier frontend 
+    allow_credentials = False # Obligatorio False si usamos "*"
+
+    settings.base_url = f"http://{host}:{settings.app_port}"
+
+elif settings.env_mode == "production":
     # En producción, si base_url está vacío en el .env, lanzamos un error preventivo
     if not settings.base_url:
         raise ValueError("ERROR: BASE_URL debe estar definida en el .env para producción")
